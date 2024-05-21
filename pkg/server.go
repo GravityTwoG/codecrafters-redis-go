f67932d6feb5630f4ee9fc26e5428b0281144bc6
GravityTwoG <crytekov@gmail.com>
2024-05-21 19:18:51 +0700

    feat: response to PING

--- pkg/server.go
+++ pkg/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
 )
 
 type redisServer struct {
@@ -25,9 +26,39 @@ func (r *redisServer) Start() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+
+	wg := &sync.WaitGroup{}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			break
+		}
+		wg.Add(1)
+		go func() {
+			r.handleConnection(conn)
+			defer conn.Close()
+			defer wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
+
+func (r *redisServer) handleConnection(conn net.Conn) {
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error reading from connection: ", err.Error())
+		return
+	}
+
+	command := string(buf[:n])
+	fmt.Println("Received command: ", command)
+
+	if command == "PING" {
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
