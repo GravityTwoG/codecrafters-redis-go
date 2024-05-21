b20605b4c20723f2b3ff021d262288d6ca327c41
GravityTwoG <crytekov@gmail.com>
2024-05-21 19:37:19 +0700

    fix: slice len, extract specifiers

--- pkg/server.go
+++ pkg/server.go
@@ -8,6 +8,12 @@ import (
 	"sync"
 )
 
+const SIMPLE_STRING_SPECIFIER = '+'
+const ERROR_SPECIFIER = '-'
+const INTEGER_SPECIFIER = ':'
+const BULK_STRING_SPECIFIER = '$'
+const ARRAY_SPECIFIER = '*'
+
 type redisServer struct {
 	host string
 	port string
@@ -51,8 +57,8 @@ func (r *redisServer) handleConnection(conn net.Conn) {
 
 	reader := bufio.NewReader(conn)
 
-	b, err := reader.ReadByte()
-	if err != nil || b != '*' {
+	firstByte, err := reader.ReadByte()
+	if err != nil || firstByte != ARRAY_SPECIFIER {
 		fmt.Println("Error reading byte: ", err.Error())
 		return
 	}
@@ -62,16 +68,21 @@ func (r *redisServer) handleConnection(conn net.Conn) {
 		fmt.Println("Error reading bytes: ", err.Error())
 		return
 	}
-	stringsCountString = stringsCountString[:len(stringsCountString)-2] // remove '\r'
+	stringsCountString = stringsCountString[:len(stringsCountString)-1] // remove '\r'
 	reader.Discard(1)                                                   // '\n'
 	fmt.Printf("Strings count: %s\n", stringsCountString)
 
+	firstByte, err = reader.ReadByte()
+	if err != nil || firstByte != BULK_STRING_SPECIFIER {
+		fmt.Println("Error reading byte: ", err.Error())
+		return
+	}
 	bulkStringLen, err := reader.ReadBytes('\r')
 	if err != nil {
 		fmt.Println("Error reading bytes: ", err.Error())
 		return
 	}
-	bulkStringLen = bulkStringLen[:len(bulkStringLen)-2] // remove '\r'
+	bulkStringLen = bulkStringLen[:len(bulkStringLen)-1] // remove '\r'
 	reader.Discard(1)                                    // '\n'
 	fmt.Printf("Bulk string length: %s\n", bulkStringLen)
 
@@ -80,7 +91,7 @@ func (r *redisServer) handleConnection(conn net.Conn) {
 		fmt.Println("Error reading bytes: ", err.Error())
 		return
 	}
-	command = command[:len(command)-2] // remove '\r'
+	command = command[:len(command)-1] // remove '\r'
 	reader.Discard(1)                  // '\n'
 	fmt.Println("Received command: ", command)
 
