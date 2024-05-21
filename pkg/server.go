efee824942ec4e08ac2b82e398dcaf779cf38e6c
GravityTwoG <crytekov@gmail.com>
2024-05-21 20:11:55 +0700

    feat: respond to echo

--- pkg/server.go
+++ pkg/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -64,46 +66,101 @@ func (r *redisServer) handleConnection(conn net.Conn) {
 	}
 }
 
-func (r *redisServer) handleCommand(reader *bufio.Reader, writer *bufio.Writer) {
+func parseParametersCount(reader *bufio.Reader) int {
 	firstByte, err := reader.ReadByte()
 	if err != nil || firstByte != ARRAY_SPECIFIER {
 		fmt.Println("Error reading byte: ", err.Error())
-		return
+		return -1
 	}
-
-	stringsCountString, err := reader.ReadBytes('\r')
+	parametersCountStr, err := reader.ReadString('\r')
 	if err != nil {
 		fmt.Println("Error reading bytes: ", err.Error())
-		return
+		return -1
 	}
-	stringsCountString = stringsCountString[:len(stringsCountString)-1] // remove '\r'
+	parametersCountStr = parametersCountStr[:len(parametersCountStr)-1] // remove '\r'
 	reader.Discard(1)                                                   // '\n'
-	fmt.Printf("Strings count: %s\n", stringsCountString)
+	fmt.Printf("Strings count: %s\n", parametersCountStr)
 
-	firstByte, err = reader.ReadByte()
+	count, err := strconv.Atoi(parametersCountStr)
+	if err != nil {
+		fmt.Println("Error parsing string: ", err.Error())
+		return -1
+	}
+	return count
+}
+
+func parseBulkStringLen(reader *bufio.Reader) int {
+	firstByte, err := reader.ReadByte()
 	if err != nil || firstByte != BULK_STRING_SPECIFIER {
 		fmt.Println("Error reading byte: ", err.Error())
-		return
+		return -1
 	}
-	bulkStringLen, err := reader.ReadBytes('\r')
+	bulkStringLen, err := reader.ReadString('\r')
 	if err != nil {
 		fmt.Println("Error reading bytes: ", err.Error())
-		return
+		return -1
 	}
 	bulkStringLen = bulkStringLen[:len(bulkStringLen)-1] // remove '\r'
 	reader.Discard(1)                                    // '\n'
 	fmt.Printf("Bulk string length: %s\n", bulkStringLen)
 
-	command, err := reader.ReadString('\r')
+	length, err := strconv.Atoi(bulkStringLen)
 	if err != nil {
-		fmt.Println("Error reading bytes: ", err.Error())
+		fmt.Println("Error parsing bulk string: ", err.Error())
+		return -1
+	}
+	return length
+}
+
+func parseCommand(reader *bufio.Reader) []string {
+	var command []string
+	parametersCount := parseParametersCount(reader)
+	if parametersCount == -1 {
+		return nil
+	}
+
+	for i := 0; i < parametersCount; i++ {
+		bulkStringLen := parseBulkStringLen(reader)
+		if bulkStringLen == -1 {
+			return nil
+		}
+
+		str, err := reader.ReadString('\r')
+		if err != nil {
+			fmt.Println("Error reading bytes: ", err.Error())
+			return nil
+		}
+		str = str[:len(str)-1] // remove '\r'
+		reader.Discard(1)      // '\n'
+		fmt.Println("Received string: ", str)
+
+		command = append(command, str)
+	}
+
+	return command
+}
+
+func writeSimpleString(writer *bufio.Writer, str string) {
+	writer.Write([]byte{'+'})
+	writer.Write([]byte(str + "\r\n"))
+}
+
+func writeBulkString(writer *bufio.Writer, str string) {
+	writer.Write([]byte{'$'})
+	writer.Write([]byte(strconv.Itoa(len(str)) + "\r\n"))
+	writer.Write([]byte(str + "\r\n"))
+}
+
+func (r *redisServer) handleCommand(reader *bufio.Reader, writer *bufio.Writer) {
+	command := parseCommand(reader)
+	if len(command) == 0 {
 		return
 	}
-	command = command[:len(command)-1] // remove '\r'
-	reader.Discard(1)                  // '\n'
-	fmt.Println("Received command: ", command)
+	commandName := strings.ToUpper(command[0])
 
-	if command == "PING" {
-		writer.Write([]byte("+PONG\r\n"))
+	if commandName == "PING" {
+		writeSimpleString(writer, "PONG")
+	} else if commandName == "ECHO" {
+		writeBulkString(writer, command[1])
 	}
 }
