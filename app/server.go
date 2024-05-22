89ebc0fa3937b8d3e88ebec3bae3f87e91c553b4
GravityTwoG <crytekov@gmail.com>
2024-05-22 12:16:49 +0700

    feat: port argument

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	redis "github.com/codecrafters-io/redis-starter-go/pkg/redis"
@@ -9,6 +10,10 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	redisServer := redis.NewRedisServer("localhost", "6379")
+	var port string
+	flag.StringVar(&port, "port", "6379", "port to listen on")
+	flag.Parse()
+
+	redisServer := redis.NewRedisServer("localhost", port)
 	redisServer.Start()
 }
